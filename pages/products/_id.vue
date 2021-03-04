08fba698ed937350558896e4d36b2366aad44e85
Daniel Acosta <d4ni3L_15@hotmail.com>
2021-03-03 22:16:05 -0500

    show example products fake api

---
+++ pages/products/_id.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    {{ product.content }}
+  </div>
+</template>
+<script>
+export default {
+  props: ['id'],
+  async asyncData({ $axios, params }) {
+    console.log(params)
+    const product = await $axios.$get(
+      `https://my-json-server.typicode.com/iosamuel/demo/products/${params.id}`
+    )
+    return { product }
+  },
+}
+</script>
---
+++ pages/products/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container mx-auto">
+    <h1>xd</h1>
+    <br />
+    <ul class="list-disc">
+      <NuxtLink
+        v-for="product in products"
+        :key="product.id"
+        :to="`/products/${product.id}`"
+        id="product.id"
+      >
+        {{ product.content }}</NuxtLink
+      >
+    </ul>
+    <br />
+    <button class="button--grey" @click="refresh">Refresh</button>
+  </div>
+</template>
+<script>
+export default {
+  async asyncData({ $axios }) {
+    const products = await $axios.$get(
+      'https://my-json-server.typicode.com/iosamuel/demo/products'
+    )
+    return { products }
+  },
+  mounted() {
+    window.onNuxtReady(() => {
+      // Podemos agregar más scripts luego de que ya cargo
+      console.log('Nuxt.js is ready and mounted')
+    })
+  },
+  methods: {
+    refresh() {
+      this.$nuxt.refresh()
+    },
+  },
+}
+</script>
